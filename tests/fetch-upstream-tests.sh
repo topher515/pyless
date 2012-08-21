bdbd4e192ad2101d287b6a4795c493ac26267895
Chris Wilcox <topher515@gmail.com>
2012-08-21 00:10:32 -0700

    Import tests from less-js upstream source; write script to checkout again

---
+++ tests/fetch-upstream-tests.sh
@@ -0,0 +1,19 @@
+repogit="https://github.com/cloudhead/less.js"
+
+
+repodir=".lessjs-master";
+
+# Clone repo if it doesnt exist
+if [ ! -d "$repodir" ]
+then
+  git clone $repogit $repodir
+fi
+
+# Fetch and merge latest changes
+pushd "$repodir";
+git fetch origin;
+git merge origin/master;
+
+# Extract tests
+git checkout-index -q --prefix="../upstream/" -- test/css/* test/less/{errors,import,*}/*;
+popd;
